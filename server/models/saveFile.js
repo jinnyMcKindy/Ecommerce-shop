bb897a1a85b9c0345af5760a3d6cebf41306970a
Kuznetsova Yulia <u.kuznetsova@iml.ru>
2019-05-24 13:03:09 +0300

    save file

---
+++ server/models/saveFile.js
@@ -0,0 +1,50 @@
+const request = require('request');
+const fs = require('fs');
+const HTMLParser = require('node-html-parser');
+
+const fileName = './server/items.txt';
+const db = require('./database');
+
+const url_phones = 'https://ru.aliexpress.com/category/202001195/mobile-phones.html?site=rus&isrefine=y';
+const url_laptop = 'https://ru.aliexpress.com/af/category/202000104.html?site=rus&d=n&catName=laptops&CatId=202000104&origin=n&isViewCP=y&jump=afs';
+const url_iphones = 'https://ru.aliexpress.com/af/category/202001195.html?isrefine=y&site=rus&brandValueIds=105790%2C3426%2C200702996&d=n&origin=n&spm=a2g0v.search0603.0.0.30f4411aQMaOBZ&jump=afs&CatId=202001195&catName=mobile-phones&isViewCP=y';
+const url_acces = 'https://ru.aliexpress.com/category/202040726/mobile-phone-accessories.html?site=rus&isrefine=y';
+
+const products = new Promise((resolve) => {
+  request(url_iphones, (error, response, body) => {
+	    if (error) reject('error:', error);
+	  	// writeFile(body)
+	  	resolve();
+  });
+});
+
+function writeFile(body) {
+  const root = HTMLParser.parse(body);
+  const lazyLoad = root.querySelector('#list-items').innerHTML;
+  const len = root.querySelectorAll('#list-items li').length;
+  console.log(len);
+  fs.writeFile(fileName, `${lazyLoad}`, (err) => {
+	    if (err) {
+	        return console.log(err);
+	    }
+	    console.log('The file was saved!');
+  });
+}
+
+function parseProduct(res) {
+  const root = HTMLParser.parse(res);
+  const li = root.querySelectorAll('li');
+  const tt = li.map((v) => {
+    const price = v.querySelector('.value').innerHTML;
+    const imageSrc = v.querySelector('.picCore').attributes['image-src'];
+    const productLink = v.querySelector('.product').attributes.href;
+    const { title } = v.querySelector('.product').attributes;
+    return {
+      price, title, imageSrc, productLink,
+    };
+  });
+  // console.log(tt)
+  return tt;
+}
+
+exports.getContent = getContent;
