4daa4669450f6c23a99b50e8297d5f1a8664076a
Kuznetsova Yulia <volokolamskspb@gmail.com>
2019-10-20 16:34:11 +0300

    move styles to less

--- client/src/components/pagination/Pagination.vue
+++ client/src/components/pagination/Pagination.vue
@@ -157,38 +157,6 @@ export default {
   },
 };
 </script>
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.page.active {
-    color: red;
-}
-.container {
-    margin: auto;
-    max-width: 700px;
-}
-.c2c-pagination {
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-}
-.page {
-    padding: 10px;
-    cursor: pointer;
-}
-.c2c-dot {
-    cursor: pointer;
-}
+<style lang="less" scoped>
+  @import "pagination.less";
 </style>
